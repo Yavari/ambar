7c45b1bb3ce8f3b306ee42dcac5cd3f2a883f2c1
Payam Yavari <payam@yavtech.se>
2019-08-30 08:25:33 +0000

    Add script to setup hostD

---
+++ setup-environment.sh
@@ -0,0 +1,7 @@
+sysctl -w vm.max_map_count=262144
+sysctl -w net.ipv4.ip_local_port_range="15000 61000"
+sysctl -w net.ipv4.tcp_fin_timeout=30
+sysctl -w net.core.somaxconn=1024
+sysctl -w net.core.netdev_max_backlog=2000
+sysctl -w net.ipv4.tcp_max_syn_backlog=2048
+sysctl -w vm.overcommit_memory=1
